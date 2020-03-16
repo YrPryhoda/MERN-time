c8f95440b193e3719f8fec7b79761d0a9c6a811a
Yaroslav <encored@i.ua>
2020-03-16 19:01:20 +0200

    created api

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+connectDB();
 app.get('/', (req, res) => {
   res.send('Server alive!');
 });
+//Define routes 
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
 
